de325b8fdd15cc101c599413d6df9197ca539ad7
Darsh-KP <133917099+Darsh-KP@users.noreply.github.com>
2024-04-09 19:18:11 -0400

    SQL credentials setup

--- src/main/java/com/buyme/database/mySQLDatabase.java
+++ src/main/java/com/buyme/database/mySQLDatabase.java
@@ -1,33 +1,45 @@
 package com.buyme.database;
 
+import com.sun.tools.javac.Main;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class mySQLDatabase {
 	public static void main(String[] args) {
 		try {
+			Properties mySQLCredentials = new Properties();
+			mySQLCredentials.load(new FileInputStream("./src/main/java/com/buyme/database/mySQL.properties"));
+
 			Connection connection = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/test",
-					"root",
-					"etheman63"
+					mySQLCredentials.getProperty("url"),
+					mySQLCredentials.getProperty("username"),
+					mySQLCredentials.getProperty("password")
 					);
-			
+
+			System.out.println(connection);
+
 			Statement statement = connection.createStatement();
 			
-			ResultSet resultSet = statement.executeQuery("select * from user");
+			ResultSet resultSet = statement.executeQuery("select * from testlogin");
 			
 			while (resultSet.next()) {
-				System.out.println(resultSet.getInt("id"));
 				System.out.println(resultSet.getString("name"));
+				System.out.println(resultSet.getString("password"));
 			}
 			
 			connection.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (IOException ioE) {
+			ioE.printStackTrace();
 		}
 	}
 }
