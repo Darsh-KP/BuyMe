4b956f761a450ae5732857776ebc3bac33599a6d
Darsh-KP <133917099+Darsh-KP@users.noreply.github.com>
2024-04-09 17:30:43 -0400

    Basic Structure

---
+++ src/main/java/database/mySQLDatabase.java
@@ -0,0 +1,33 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class mySQLDatabase {
+	public static void main(String[] args) {
+		try {
+			Connection connection = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/test",
+					"root",
+					"etheman63"
+					);
+			
+			Statement statement = connection.createStatement();
+			
+			ResultSet resultSet = statement.executeQuery("select * from user");
+			
+			while (resultSet.next()) {
+				System.out.println(resultSet.getInt("id"));
+				System.out.println(resultSet.getString("name"));
+			}
+			
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
