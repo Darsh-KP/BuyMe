d65f42d4d61339bd4833c0cbbf9f03c4d4db24a6
Darsh-KP <133917099+Darsh-KP@users.noreply.github.com>
2024-04-12 22:30:52 -0400

    Finished DB class

--- src/main/java/com/buyme/database/mySQLDatabase.java
+++ src/main/java/com/buyme/database/mySQLDatabase.java
@@ -4,40 +4,39 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Properties;
 
 public class mySQLDatabase {
-	public static void main(String[] args) {
+	public mySQLDatabase() {}
+
+	public Connection newConnection() {
+		Connection connection = null;
 		try {
 			Properties mySQLCredentials = new Properties();
 			mySQLCredentials.load(new FileInputStream("./src/main/java/com/buyme/database/mySQL.properties"));
 
-			Connection connection = DriverManager.getConnection(
+			connection = DriverManager.getConnection(
 					mySQLCredentials.getProperty("url"),
 					mySQLCredentials.getProperty("username"),
 					mySQLCredentials.getProperty("password")
 					);
-
-			System.out.println(connection);
-
-			Statement statement = connection.createStatement();
-			
-			ResultSet resultSet = statement.executeQuery("select * from testlogin");
-			
-			while (resultSet.next()) {
-				System.out.println(resultSet.getString("name"));
-				System.out.println(resultSet.getString("password"));
-			}
-			
-			connection.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException ioE) {
-			ioE.printStackTrace();
+			System.out.println("Couldn't read database credentials.");
+			//ioE.printStackTrace();
+		}
+
+		return connection;
+	}
+
+	public void CloseConnection(Connection connection) {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
 }
