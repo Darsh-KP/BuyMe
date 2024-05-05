bef10d1c4bd9ec943eb81dd12feba5f36b6afa56
ome14336 <166339508+ome14336@users.noreply.github.com>
2024-05-05 12:19:57 -0400

    Update ticketController.java

--- src/main/java/com/buyme/controller/ticketController.java
+++ src/main/java/com/buyme/controller/ticketController.java
@@ -70,6 +70,68 @@ public class ticketController {
             return ticketDisplay;
         } catch (SQLException e) {
             if (myDatabase.debug) {
+                System.out.println("Error getting all tickets for user...");
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+    
+    public static List<HashMap<String, String>> getAllTickets() {
+        
+    	try {
+            // Create connection
+    		myDatabase database = new myDatabase();
+    	    Connection ticketConnection = database.newConnection();
+
+    	    // Prepare the SQL statement for execution
+    	    PreparedStatement statement = ticketConnection.prepareStatement(
+    	        "SELECT ticket_ID, created_by, comment, created_at FROM tickets");
+    	    
+    	    // Execute the query
+    	    ResultSet resultSet = statement.executeQuery();
+            
+
+            // Store the results
+            ArrayList<HashMap<String, String>> ticketDisplay = new ArrayList<HashMap<String, String>>();
+            while (resultSet.next()) {
+                // Get each listing
+                HashMap<String, String> ticket = new HashMap<String, String>();
+                //ticket ID
+                int ticketID = resultSet.getInt("ticket_id");
+                ticket.put("ticketID", String.valueOf(ticketID));
+                
+                
+                //created_by 
+                ticket.put("created_by", resultSet.getString("created_by"));
+
+                // Comment
+                String comment = resultSet.getString("comment");
+                if (comment.length() > 25) {
+                    comment = comment.substring(0, 22).trim() + "...";
+                }
+                ticket.put("comment", comment);
+                
+
+                // creation DT
+                LocalDateTime creationDateTime = resultSet.getTimestamp("created_at").toLocalDateTime();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd, yyyy 'at' HH:mm:ss");
+                String formattedDateTime = creationDateTime.format(formatter);
+                ticket.put("creation", formattedDateTime);
+                
+                ticketDisplay.add(ticket);
+            }
+
+            //Close connection
+            resultSet.close();
+            statement.close();
+            ticketConnection.close();
+
+            // Return the strings to display
+            return ticketDisplay;
+        } catch (SQLException e) {
+            if (myDatabase.debug) {
                 System.out.println("Error getting all tickets...");
                 e.printStackTrace();
             }
